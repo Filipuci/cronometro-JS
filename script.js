ef390c09cfbf64347ce3add9a592ffa5b8fb5062
Filipuci <rafaellifipuci@outlook.com>
2025-06-29 22:46:58 -0300

    improving script.js

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
+const secondsElement = document.querySelector('#seconds')
+const minutesElement = document.querySelector('#minutes')
+const hoursElement = document.querySelector('#hours')
+
 let seconds = 0
 let minutes = 0
 let hours = 0
 
-let myInterval = null
+let timer = null
 
 document.querySelector('.play').addEventListener('click', () => {
-  if (myInterval !== null) return
-  myInterval = setInterval(incrementTime, 1000)
+  if (timer !== null) return
+  timer = setInterval(incrementTime, 1000)
 })
 
 document.querySelector('.stop').addEventListener('click', () => {
-  clearInterval(myInterval)
-  myInterval = null
+  clearInterval(timer)
+  timer = null
 })
 
 document.querySelector('.reset').addEventListener('click', () => {
+  clearInterval(timer)
+  timer = null
+
   seconds = 0
   minutes = 0
   hours = 0
-
   updateDisplay()
-  clearInterval(myInterval)
-  myInterval = null
 })
 
 const incrementTime = () => {
@@ -39,9 +43,9 @@ const incrementTime = () => {
 }
 
 const updateDisplay = () => {
-  document.querySelector('#seconds').textContent = formatTwoDigits(seconds)
-  document.querySelector('#minutes').textContent = formatTwoDigits(minutes)
-  document.querySelector('#hours').textContent = formatTwoDigits(hours)
+  secondsElement.textContent = formatTwoDigits(seconds)
+  minutesElement.textContent = formatTwoDigits(minutes)
+  hoursElement.textContent = formatTwoDigits(hours)
 }
 
-const formatTwoDigits = number => number.toString().padStart(2, '0')
+const formatTwoDigits = n => n.toString().padStart(2, '0')
